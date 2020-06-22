109d09072df4a0c257b236d6e73ce0e92fef66b8
Fabricio Tello <ftello@puntoticket.cl>
2020-06-22 11:15:34 -0400

    agrega nuevos modulos

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+	imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule],
+	exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule],
 })
 export class MaterialModule {}
