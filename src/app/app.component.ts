07fa017888903a8d05146b024038c6e376dfeb04
Fabricio Tello <ftello@puntoticket.cl>
2020-06-24 19:56:07 -0400

    agrega servicio y socket.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { ThemeService } from './theme/theme.service';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'drogueria-frontend';
+	title = 'drogueria-frontend';
 
-  constructor(
-    private translate: TranslateService,
-    private router: Router,
-    private themeService: ThemeService
-  ) {
-    this.translate.setDefaultLang('es');
-    this.translate.use('es');
-  }
+	constructor(
+		private translate: TranslateService,
+		private router: Router,
+		private themeService: ThemeService,
+		private websocketService: WebsocketService
+	) {
+		this.translate.setDefaultLang('es');
+		this.translate.use('es');
+	}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {
+		this.websocketService.listen('bienvenido').subscribe((data) => {
+			console.log(data);
+		});
+	}
 }
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+	providedIn: 'root',
+})
+export class WebsocketService {
+
+	socket: any;
+	readonly uri: string = 'ws://localhost:8081';
+
+	constructor() {
+		this.socket = io(this.uri);
+	}
+
+
+	listen(eventName: string){
+		return new Observable((subscriber) => {
+			this.socket.on(eventName, (data) => {
+				subscriber.next(data);
+			});
+		});
+	}
+
+	emit(eventName: string, data: any){
+		this.socket.emit(eventName, data);
+	}
+}
