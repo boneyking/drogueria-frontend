a82e4e836345e02e1a7dd3086bf319b2948cc6af
Fabricio Tello <ftello@puntoticket.cl>
2020-06-23 20:08:02 -0400

    agrega propiedad para emitar cuando se cambie texto en input

--- src/app/shared/components/formulario/input/input.component.ts
+++ src/app/shared/components/formulario/input/input.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+
 
 @Component({
 	selector: 'app-input',
@@ -14,7 +15,13 @@ export class InputComponent implements OnInit {
 	@Input() etiqueta: string;
 	@Input() hint: string;
 
+	@Output() valorInput = new EventEmitter<string>();
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	textoCambiado(texto: string): void{
+		this.valorInput.emit(texto);
+	}
 }
